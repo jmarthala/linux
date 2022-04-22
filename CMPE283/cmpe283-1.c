6cbdee07da6c32fcb89bccce016b7414a3f924af
Jahnavi <jahnavi.marthala@sjsu.edu>
2022-04-22 12:55:22 -0700

    executable files

--- CMPE283/cmpe283-1.c
+++ CMPE283/cmpe283-1.c
@@ -9,10 +9,13 @@
 
 /*
  * Model specific registers (MSRs) by the module.
- * See SDM volume 4, section 2.1
+ *From SDM
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
-
+#define IA32_VMX_PROCBASED_CTLS 0x482
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+#define IA32_VMX_EXIT_CTLS 0x483
+#define IA32_VMX_ENTRY_CTLS 0x484
 /*
  * struct caapability_info
  *
@@ -37,6 +40,115 @@ struct capability_info pinbased[5] =
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
 };
+
+/* from SDM Table 24-6. */
+struct capability_info procbased[21]=
+{
+        { 2, " Interrupt-window exiting"},
+        {3,"Use TSC offsetting" },
+        {7, "HLT exiting"},
+        {9,"INVLPG exiting"},
+        {10,"MWAIT exiting"},
+        {11," RDPMC exiting"},
+        {12," RDTSC exiting"},
+        {15,"CR3-load exiting"},
+        { 16, "CR3-store exiting" },
+{ 19, "CR8-load exiting" },
+{ 20, "CR8-store exiting" },
+{ 21, "Use TPR shadow " },
+{ 22, "NMI-window exiting" },
+{ 23, "MOV-DR exiting" },
+{ 24, "Unconditional I/O exiting" },
+{ 25, "Use I/O bitmaps " },
+{ 27, "Monitor trap flag " },
+{ 28, "Use MSR bitmaps" },
+{ 29, "MONITOR exiting" },
+{ 30, "PAUSE exiting" },
+{31, " Activate secondary controls"}
+};
+
+
+/*   From SDM Table 24-7.         */
+
+struct capability_info secondary_procbased[27] =
+{
+	{0,"Virtualize APIC accesses" },
+	{1, "Enable EPT"},
+	{2," Descriptor-table exiting"},
+	{3, " Enable RDTSCP"},
+	{4, "Virtualize x2APIC mode"},
+	{5, " Enable VPID"},
+	{6,"WBINVD exiting"},
+	{7, "Unrestricted guest"},
+	{8, "APIC-register virtualization"},
+	{9,"Virtual-interrupt delivery"},
+	{ 10, "PAUSE-loop exiting"},
+	{11, "RDRAND exiting "},
+	{12, "Enable INVPCID"},
+	{13, " Enable VM functions"},
+	{14, "VMCS shadowing"},
+	{15, "Enable ENCLS exiting"},
+	{16, "Enable ENCLS exiting"},
+	{17, "Enable PML"},
+	{18, "EPT-violation #VE"},
+	{19,"Conceal VMX from PT"},
+	{20, " Enable XSAVES/XRSTORS"},
+	{22,"  Mode-based execute control for EPT"},
+	{23,"Sub-page write permissions for EPT"},
+	{24, "Intel PT uses guest physical addresses"},
+	{25, "Use TSC scaling"},
+	{26, "Enable user wait and pause"},
+	{28,"Enable ENCLV exiting"}
+
+};
+
+
+/* From SDM Table Table 24-11. */
+
+struct capability_info exit_control[14] =
+{
+
+	{2, " Save debug controls"},
+	{9, "Host address-spac size"},
+	{12, "Load IA32_PERF_GLOBAL_CTRL"},
+	{15, " Acknowledge interrupt on exit"},
+	{18, "Save IA32_PAT"},
+	{19," Load IA32_PAT"},
+	{20,"Save IA32_EFER"},
+	{21,"Load IA32_EFER"},
+	{22, " Save VMXpreemption timer value"},
+	{23, "Clear IA32_BNDCFGS"},
+	{24," Conceal VMX from PT"},
+	{ 25, "Clear IA32_RTIT_CTL"},
+	{28, "Load CET state"},
+	{29,"Load PKRS"}
+
+};
+
+
+/* From SDM Table 24-13. */
+
+struct capability_info entry_control[12]=
+{
+ 	{2, " Load debug controls"},
+	{9, "IA-32e mode guest"},
+	{10,"Entry to SMM"},
+	{11, " Deactivate dualmonitor treatment"},
+	{13, " Load IA32_PERF_GLOBAL_CTRL "},
+	{14, " Load IA32_PAT"},
+	{15, " Load IA32_EFER"},
+	{16, "Load IA32_BNDCFGS"},
+	{17, " Conceal VMX from PT"},
+	{ 18, "Load IA32_RTIT_CTL" },
+	{ 20, "Load CET state" },
+	{ 22, "Load PKRS" }
+
+};
+
+
+
+ 
+
 
 /*
  * report_capability
@@ -85,6 +197,41 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+
+
+
+        /* Primary proc based control*/
+       rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+	pr_info("Primary Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased, 21, lo, hi);
+
+
+
+	/*Secondary proc based control*/
+	rdmsr(IA32_VMX_PROCBASED_CTLS2, lo, hi);
+	pr_info(" Secondary Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(secondary_procbased, 27, lo, hi);
+
+
+
+	/* Exit control*/
+
+	rdmsr ( IA32_VMX_EXIT_CTLS, lo, hi);
+	pr_info(" Exit Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(exit_control, 14, lo, hi);
+
+
+	/* Entry control */
+
+        rdmsr(IA32_VMX_ENTRY_CTLS , lo, hi);
+	pr_info(" Entry Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(entry_control, 12, lo, hi);
+
+
 }
 
 /*
@@ -118,5 +265,4 @@ cleanup_module(void)
 {
 	printk(KERN_INFO "CMPE 283 Assignment 1 Module Exits\n");
 }
-
 MODULE_LICENSE("GPL v2");
